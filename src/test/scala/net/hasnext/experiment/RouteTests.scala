57f05a0dd21069d75261dd625856be35aa71640c
Andrew Browne <brownie@brownie.com.au>
2011-03-23 23:51:33 +1100

    Added start of routing

---
+++ src/test/scala/net/hasnext/experiment/RouteTests.scala
@@ -0,0 +1,43 @@
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+trait Route {
+  def getUrl(inputModel : AnyRef) : Option[String]
+}
+
+class MyRoute extends Route {
+  import scala.collection.mutable.HashMap 
+  val routeTable = new HashMap[Class[_],String]
+  def getUrl(inputModel : AnyRef) = {
+      for(i <- routeTable.keys)
+        println(i)
+      println(routeTable.keys)
+      routeTable get inputModel.getClass
+  }
+
+  def register(inputModel : Class[_], url : String){
+    routeTable += (inputModel -> url)
+  }
+}
+
+class MyInputModel
+
+class RouteSpec extends Spec with ShouldMatchers {
+  describe("A route") {
+    describe("(when empty)") {
+      val myRoute = new MyRoute
+      it("should return blank") {
+        myRoute.getUrl(new Object) should be (None)
+      }
+    }
+
+    describe("(when registered)") {
+      val myRoute = new MyRoute
+      myRoute.register(classOf[MyInputModel], "/inputtest")
+      it("should return url") {
+        myRoute.getUrl(new MyInputModel) should be (Some("/inputtest"))
+      }
+    }
+  }
+}
+
